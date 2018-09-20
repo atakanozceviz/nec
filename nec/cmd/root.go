4c2aafd546ae0ac813965e148832bb68de13dda2
Atakan Özceviz <atakanozceviz@hotmail.com>
2018-09-20 11:58:57 +0300

    added descriptions for cli

--- nec/cmd/root.go
+++ nec/cmd/root.go
@@ -28,13 +28,12 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "nec",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Build and test necessary VS projects.",
+	Long: `nec creates dependency graph for VS projects
+then findsout which VS projects needs to build
+and their tests to run by checking a git commit 
+after that executes user-defined commands.
+`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
