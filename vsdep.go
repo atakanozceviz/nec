8e16320142a91d86cfbb2c711bd72394c1a2e840
Atakan Özceviz <atakanozceviz@hotmail.com>
2018-09-24 17:46:31 +0300

    Removed unnecessary checks

--- vsdep.go
+++ vsdep.go
@@ -1,20 +1,12 @@
 package nec
 
 import (
-	"os"
 	"path"
-	"strings"
 
 	"github.com/atakanozceviz/vsdep"
 )
 
 func findPaths(settings *Settings, lastcommit string, walkpath ...string) error {
-	wd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	projectFolderName := "/" + path.Base(wd) + "/"
-
 	paths, err := vsdep.FindOut(lastcommit, walkpath...)
 	if err != nil {
 		return err
@@ -23,24 +15,12 @@ func findPaths(settings *Settings, lastcommit string, walkpath ...string) error 
 		if settings.Paths == nil {
 			settings.Paths = make(map[string][]string)
 		}
-		if len(walkpath) >= 1 && walkpath[0] != "." {
-			if strings.Contains(path.Dir(pth), projectFolderName) {
-				settings.Paths["test"] = append(settings.Paths["test"], path.Dir(pth))
-			}
-			continue
-		}
 		settings.Paths["test"] = append(settings.Paths["test"], path.Dir(pth))
 	}
 
 	for _, pth := range paths.Solutions {
 		if settings.Paths == nil {
 			settings.Paths = make(map[string][]string)
-		}
-		if len(walkpath) >= 1 && walkpath[0] != "." {
-			if strings.Contains(path.Dir(pth), projectFolderName) {
-				settings.Paths["build"] = append(settings.Paths["build"], path.Dir(pth))
-			}
-			continue
 		}
 		settings.Paths["build"] = append(settings.Paths["build"], path.Dir(pth))
 	}
